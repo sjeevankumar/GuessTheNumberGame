570d0bcc55e7e67f8e1f38acc19b48ed81a05b83
Jeevan <v.c.s.jeevan.vadde.143@gmail.com>
2021-05-30 10:39:52 +0530

    Commit 2 wow i wondered how easy it is compared to commit 1

--- index.js
+++ index.js
@@ -1,54 +1,16 @@
-// STep1 :- Generating a Random number using math library
-let upper=100;
-let lower=1;
-let myrandom=Math.floor(Math.random()*(upper-lower+1)+lower);
-// let myrandom=80;
+let output=document.getElementById('outputtext');
+let number=[Math.floor(Math.random()*100)];
 
+btn.addEventListener('click',()=>{
+      let input=document.getElementById('userInput').value;
+      if(input==number){
+            output.innerText=`You guessed right, your number was ${number}`
+      }
+      else if(input<number){
+            output.innerText=`You guessed to low!`
+      }
+      if(input>number){
+            output.innerText='You gussed to high!'
+      }
 
-// Step2:- Getting a user input
-let input=document.getElementById('txt');
-let userInput;
-let result=document.getElementById('result');
-let btn=document.getElementById('btn');
-input.addEventListener('keyup',()=>{
-      userInput=input.value;
-      // console.log(userInput);
-      //Step3:- Checking userinput number and random number are same or not using add button
-      btn.addEventListener('click',()=>{
-            let b=0;
-            while (b==0) {
-                  if (myrandom==userInput) {
-                        let para=document.createElement('p');
-                        para.innerText='Correct';
-                        result.appendChild(para);
-                        console.log('Correct');
-                        b=b+1;
-                        break;
-                  }
-                  else if(myrandom<userInput){
-                        let para=document.createElement('p');
-                        para.innerText='High';
-                        result.appendChild(para);
-                        console.log('High')
-                        break;
-                  }
-                  else if(myrandom>userInput){
-                        let para=document.createElement('p');
-                        para.innerText='low';
-                        result.appendChild(para);
-                        // console.log('Low');
-                        break;
-                  }
-            }
-      })
-            
-});
-
-
-
-
-
-
-
-
-
+}) 
